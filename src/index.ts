3e7cf7f3873e84fcca099a061b30b53fce3be43f
Sandesh Hyoju <sundesz@gmail.com>
2023-01-01 11:32:54 +0200

    Add dotenv package

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import http from "http";
 import app from "./app";
+import { PORT } from "./config";
 
 const server = http.createServer(app);
-const PORT = 8080;
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
