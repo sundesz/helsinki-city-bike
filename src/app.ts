627c88ed59e2ffc6734cf60878298e32e428d071
Sandesh Hyoju <sundesz@gmail.com>
2023-01-03 00:34:34 +0200

    Add routes and error handling feature

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, Request, Response } from 'express';
+import journeyRoutes from './api/routes/journeyRoutes';
+import stationRoutes from './api/routes/stationRoutes';
 
 const app: Application = express();
 
 app.use(express.json());
 
-app.get("/ping", (_req: Request, res: Response) => {
-  res.send("pong");
+app.get('/ping', (_req: Request, res: Response) => {
+  res.send('pong');
 });
+
+app.use(journeyRoutes);
+app.use(stationRoutes);
 
 export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,72 +1,16 @@
 import http from 'http';
 import app from './app';
 import { PORT } from './config';
-// import { connectToDB } from './db';
-import fs, { opendirSync } from 'fs';
-import readline from 'readline';
-import { FileTypes } from './types';
-import { getFileType, parseRow } from './utils';
+import { connectToDB } from './db';
+import { listAllCSVFiles } from './utils';
 
 const server = http.createServer(app);
 
-// fileName with path
-const readFile = (fileName: string) => {
-  const stream = fs.createReadStream(fileName);
-  const rl = readline.createInterface({ input: stream });
-
-  const data: string[][] = [];
-  let firstLine = true;
-  let fileType: FileTypes | undefined = undefined;
-
-  rl.on('line', (row) => {
-    if (firstLine) {
-      fileType = getFileType(row);
-      firstLine = false;
-      return;
-    }
-
-    if (fileType === undefined) {
-      return;
-    }
-
-    const parsedRow = parseRow(fileType, row);
-
-    if (parsedRow) {
-      data.push(parsedRow);
-    }
-  });
-
-  rl.on('close', () => {
-    console.log(data.length);
-    console.log(fileName);
-    console.log(fileType);
-    firstLine = true;
-
-    // fs.unlink(fileName, (err) => {
-    //   if (err) {
-    //     throw err;
-    //   }
-
-    //   console.log('Delete File successfully.');
-    // });
-  });
-};
-
-const listAllCSVFiles = async () => {
-  const PATH_FOR_DATA = './src/data';
-  const dir = opendirSync(PATH_FOR_DATA);
-
-  for await (const entry of dir) {
-    if (entry.isFile() && entry.name.endsWith('.csv')) {
-      // console.log('Found File: ', entry.name);
-      readFile(`${PATH_FOR_DATA}/${entry.name}`);
-    }
-  }
-};
+export const SRC_DIR = __dirname;
 
 const start = async () => {
   try {
-    // await connectToDB();
+    await connectToDB();
     await listAllCSVFiles();
     server.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
