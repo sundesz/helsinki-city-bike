63d2d80c0d1b8b6fee404b2e170532180df5f03c
Sandesh Hyoju <sundesz@gmail.com>
2023-01-08 00:44:26 +0200

    Add proxy to fetch data from server

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api/v1': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
+    },
+  },
+  build: {
+    outDir: '../dist',
+  },
+
   plugins: [react()],
-})
+});
