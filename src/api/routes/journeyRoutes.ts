70f1220037304eb4e7e38f788f278e61f2a39991
Sandesh Hyoju <sundesz@gmail.com>
2023-01-07 18:28:02 +0200

    Add a route to get one data from journey

--- src/api/routes/journeyRoutes.ts
+++ src/api/routes/journeyRoutes.ts
@@ -4,5 +4,6 @@ import journeyController from '../controllers/journeyController';
 const router = Router();
 
 router.get('/', journeyController.getAll);
+router.get('/:id', journeyController.get);
 
 export default router;
