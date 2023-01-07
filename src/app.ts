fd53c51cc90027bfbc04780e6543f67b08f5a548
Sandesh Hyoju <sundesz@gmail.com>
2023-01-07 18:37:50 +0200

    Disabling cors. Use proxy in frontend to access to server

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,23 @@
 import express, { Application, Request, Response } from 'express';
+// import cors from 'cors';
 import journeyRoutes from './api/routes/journeyRoutes';
 import stationRoutes from './api/routes/stationRoutes';
 
 const app: Application = express();
 
+// app.use(
+//   (cors as (options: cors.CorsOptions) => express.RequestHandler)({
+//     credentials: true,
+//     origin: 'http://localhost:5173',
+//   })
+// );
 app.use(express.json());
 
 app.get('/ping', (_req: Request, res: Response) => {
   res.send('pong');
 });
 
-app.use('/journey', journeyRoutes);
-app.use('/station', stationRoutes);
+app.use('/api/v1/journey', journeyRoutes);
+app.use('/api/v1/station', stationRoutes);
 
 export default app;
