5bb6813c10134200d476c092087ee489ee5dc309
Sandesh Hyoju <sundesz@gmail.com>
2023-01-12 02:09:29 +0200

    Add 404 page

---
+++ client/src/components/Layout/Page404.tsx
@@ -0,0 +1,20 @@
+import { useNavigate } from 'react-router-dom';
+
+const Page404 = () => {
+  const navigate = useNavigate();
+
+  const goBackHandler = () => {
+    navigate(-1);
+  };
+
+  return (
+    <>
+      <div className="page-404">Page not found</div>
+      <div className="go-back" onClick={goBackHandler}>
+        Back to homepage
+      </div>
+    </>
+  );
+};
+
+export default Page404;
