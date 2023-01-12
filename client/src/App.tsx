fb1953e1fb4a4868eed47e51ddedaef00fcbad51
Sandesh Hyoju <sundesz@gmail.com>
2023-01-12 02:12:56 +0200

    Add react-router-dom and react-toastify component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,37 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import Layout from './components/Layout';
 import Home from './features/Home';
-import JourneyList from './features/Journey/JourneyList';
-import StationList from './features/Station/StationList';
+import Journey from './features/Journey';
+import NewJourney from './features/Journey/NewJourney';
+import SingleJourney from './features/Journey/SingleJourney';
+import Station from './features/Station';
+import SingleStation from './features/Station/SingleStation';
+import 'react-toastify/dist/ReactToastify.css';
+import NewStation from './features/Station/NewStation';
 
 const App = () => {
   return (
     <Router>
-      <Layout>
-        <Routes>
-          <Route path="/journey" element={<JourneyList />} />
-          <Route path="/station" element={<StationList />} />
+      <ToastContainer position="top-center" autoClose={2000} />
+
+      <Routes>
+        <Route path="/" element={<Layout />}>
           <Route path="/" element={<Home />} />
-        </Routes>
-      </Layout>
+
+          <Route path="station">
+            <Route index element={<Station />} />
+            <Route path="new" element={<NewStation />} />
+            <Route path=":stationId" element={<SingleStation />} />
+          </Route>
+
+          <Route path="journey">
+            <Route index element={<Journey />} />
+            <Route path="new" element={<NewJourney />} />
+            <Route path=":journeyId" element={<SingleJourney />} />
+          </Route>
+        </Route>
+      </Routes>
     </Router>
   );
 };
