b2e4dd618f175d5e4ea8b2e8ecbbd397d9f14f36
Sandesh Hyoju <sundesz@gmail.com>
2023-01-12 02:23:53 +0200

    Change home page content

--- client/src/features/Home/index.tsx
+++ client/src/features/Home/index.tsx
@@ -1,5 +1,28 @@
+import { ListGroup } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
 const Home = () => {
-  return <div>Home Page</div>;
+  return (
+    <>
+      <div className="home-title">Welcome to Helsinki City Bike App</div>
+      <div className="text-center mb-5">
+        <ListGroup>
+          <ListGroup.Item>
+            <Link to={'/journey'}>List all Journey</Link>
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <Link to={'/journey/new'}>Create new journey</Link>
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <Link to={'/station'}>List all Station</Link>
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <Link to={'/station/new'}>Create new station</Link>
+          </ListGroup.Item>
+        </ListGroup>
+      </div>
+    </>
+  );
 };
 
 export default Home;
