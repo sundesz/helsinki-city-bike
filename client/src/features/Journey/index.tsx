558c53a359377f7feadd46b5459e35d32569139b
Sandesh Hyoju <sundesz@gmail.com>
2023-01-13 15:55:53 +0200

    Add Journey feature

---
+++ client/src/features/Journey/index.tsx
@@ -0,0 +1,80 @@
+import { useState } from 'react';
+import ErrorPage from '../../components/Layout/ErrorPage';
+import Loading from '../../components/Layout/Loading';
+import { useSearchQuery } from '../../hooks/useSearchQuery';
+import AppPagination from '../Pagination';
+import { useGetJourneyQuery } from './journeyApiSlice';
+import JourneyFilter from './JourneyFilter';
+import JourneyList from './JourneyList';
+
+const Journey = () => {
+  const { pageNumber, filterName, filterValue, orderBy, orderDir } =
+    useSearchQuery({ filterName: 'departure_station_name' });
+
+  const [page, setPage] = useState<number>(Number(pageNumber));
+  const [filterColumn, setFilterColumn] = useState<string>(filterName);
+  const [filterText, setFilterText] = useState<string>(filterValue);
+
+  console.log({
+    page,
+    filterName: filterColumn,
+    filterValue,
+    orderBy,
+    orderDir,
+  });
+
+  const {
+    data: journeyData,
+    isLoading,
+    isError,
+    error,
+  } = useGetJourneyQuery({
+    page,
+    filterName: filterColumn,
+    filterValue,
+    orderBy,
+    orderDir,
+  });
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (isError) {
+    <ErrorPage error={error} />;
+  }
+
+  if (!journeyData?.data) {
+    return <div>No data </div>;
+  }
+  console.log(journeyData);
+  return (
+    <>
+      <JourneyFilter
+        filterText={filterText}
+        filterColumn={filterColumn}
+        setFilterColumn={setFilterColumn}
+        setFilterText={setFilterText}
+        setPage={setPage}
+      />
+
+      <AppPagination
+        pageType="journey"
+        totalPage={journeyData.totalPage}
+        currentPage={journeyData.currentPage}
+        setPage={setPage}
+      />
+
+      <JourneyList data={journeyData.data} />
+
+      <AppPagination
+        pageType="journey"
+        totalPage={journeyData.totalPage}
+        currentPage={journeyData.currentPage}
+        setPage={setPage}
+      />
+    </>
+  );
+};
+
+export default Journey;
